a18229b5c6b58289f28482e5287f086bdaf858d6
kadire sindhu reddy <130839368+Sindhureddy342@users.noreply.github.com>
2023-08-10 18:37:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+function App(){
+  return(
+    <div className="main-container"> 
+      <div classname="center-container">
+      </div>
+    </div>
+
+  )
+}
+export default App
